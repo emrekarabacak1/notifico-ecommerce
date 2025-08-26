52a169280f9e9ec4ac78c67d742d471d31569d16
Emre Karabacak <emrekarabacak22@gmail.com>
2025-08-26 13:36:07 +0300

    Dashboard controller action ve ViewModel verileri eklendi

---
+++ Models/ViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Notifico.Models
+{
+    public class ViewModel
+    {
+        public int TotalOrderCount { get; set; }
+        public decimal TotalSales { get; set; }
+        public int TotalCustomerCount { get; set; }
+        public List<Order> RecentOrders { get; set; }
+    }
+}
